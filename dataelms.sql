bbc2d43f888aec80881eba53e5ce114871bbf8cd
tansovir <tanvir8321@gmail.com>
2021-01-06 08:57:18 +0600

    database sql file add

---
+++ dataelms.sql
@@ -0,0 +1,259 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 06, 2021 at 03:55 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dataelms`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `generate_due_list` ()  NO SQL
+SELECT I.issue_id, M.email, B.isbn, B.title
+FROM book_issue_log I INNER JOIN member M on I.member = M.username INNER JOIN book B ON I.book_isbn = B.isbn
+WHERE DATEDIFF(CURRENT_DATE, I.due_date) >= 0 AND DATEDIFF(CURRENT_DATE, I.due_date) % 5 = 0 AND (I.last_reminded IS NULL OR DATEDIFF(I.last_reminded, CURRENT_DATE) <> 0)$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `isbn` char(13) NOT NULL,
+  `title` varchar(80) NOT NULL,
+  `author` varchar(80) NOT NULL,
+  `copies` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`isbn`, `title`, `author`, `copies`) VALUES
+('2546932156', '	East-West : Purbo-Paschim (Book 1)', 'Sunil Gangopadhyay', 14),
+('2564875695', 'Srikanta', 'Sarat Chandra Chattopadhyay', 5),
+('3261548952', 'The Art of Computer Programming, Volume 1: Fundamentals of Algorithm', 'Donald Ervin Knuth', 14),
+('4587251694', 'Introduction to Algorithm', 'Thomas. H. Cormen', 12),
+('4587256487', 'The C Programming Language', 'Brian W. Kernighan', 3),
+('5458726158', 'Algorithms to Live By: The Computer Science of Human Decisions', 'Brian Christian, Tom Griffiths', 14),
+('9789350777077', 'The Soul of a New Machine ', 'Tracy Kidder', 3),
+('UPDATE `book`', 'Pather Panchali: Song of the Road', 'Bibhutibhushan Bandyopadhyay', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_issue_log`
+--
+
+CREATE TABLE `book_issue_log` (
+  `issue_id` int(11) NOT NULL,
+  `member` varchar(20) NOT NULL,
+  `book_isbn` varchar(13) NOT NULL,
+  `due_date` date NOT NULL,
+  `last_reminded` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Triggers `book_issue_log`
+--
+DELIMITER $$
+CREATE TRIGGER `issue_book` BEFORE INSERT ON `book_issue_log` FOR EACH ROW BEGIN
+	SET NEW.due_date = DATE_ADD(CURRENT_DATE, INTERVAL 7 DAY);
+    UPDATE member SET balance = balance - (SELECT price FROM book WHERE isbn = NEW.book_isbn) WHERE username = NEW.member;
+    UPDATE book SET copies = copies - 1 WHERE isbn = NEW.book_isbn;
+    DELETE FROM pending_book_requests WHERE member = NEW.member AND book_isbn = NEW.book_isbn;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `return_book` BEFORE DELETE ON `book_issue_log` FOR EACH ROW BEGIN
+    UPDATE member SET balance = balance + (SELECT price FROM book WHERE isbn = OLD.book_isbn) WHERE username = OLD.member;
+    UPDATE book SET copies = copies + 1 WHERE isbn = OLD.book_isbn;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `librarian`
+--
+
+CREATE TABLE `librarian` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` char(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `librarian`
+--
+
+INSERT INTO `librarian` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` char(40) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id`, `username`, `password`, `name`, `email`) VALUES
+(6, 'tanvir', 'tanvir', 'tanvir', 'tanvir@me.com');
+
+--
+-- Triggers `member`
+--
+DELIMITER $$
+CREATE TRIGGER `add_member` AFTER INSERT ON `member` FOR EACH ROW DELETE FROM pending_registrations WHERE username = NEW.username
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `remove_member` AFTER DELETE ON `member` FOR EACH ROW DELETE FROM pending_book_requests WHERE member = OLD.username
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pending_book_requests`
+--
+
+CREATE TABLE `pending_book_requests` (
+  `request_id` int(11) NOT NULL,
+  `member` varchar(20) NOT NULL,
+  `book_isbn` varchar(13) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pending_book_requests`
+--
+
+INSERT INTO `pending_book_requests` (`request_id`, `member`, `book_isbn`, `time`) VALUES
+(9, 'tanvir', '5458726158', '2020-12-21 04:58:40'),
+(10, 'tanvir', '2546932156', '2021-01-06 02:45:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pending_registrations`
+--
+
+CREATE TABLE `pending_registrations` (
+  `username` varchar(20) NOT NULL,
+  `password` char(40) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `balance` int(4) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`isbn`);
+
+--
+-- Indexes for table `book_issue_log`
+--
+ALTER TABLE `book_issue_log`
+  ADD PRIMARY KEY (`issue_id`);
+
+--
+-- Indexes for table `librarian`
+--
+ALTER TABLE `librarian`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `pending_book_requests`
+--
+ALTER TABLE `pending_book_requests`
+  ADD PRIMARY KEY (`request_id`);
+
+--
+-- Indexes for table `pending_registrations`
+--
+ALTER TABLE `pending_registrations`
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_issue_log`
+--
+ALTER TABLE `book_issue_log`
+  MODIFY `issue_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `librarian`
+--
+ALTER TABLE `librarian`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `pending_book_requests`
+--
+ALTER TABLE `pending_book_requests`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
